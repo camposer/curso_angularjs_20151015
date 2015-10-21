d92635fa37e84a8389616907b6865ee44ba594ca
Rodolfo Campos <camposer@gmail.com>
2015-10-21 20:47:20 +0200

    Solución del ejercicio3 (primera parte - crud+)

--- ejercicio3/public/javascripts/ProductoUtil.js
+++ ejercicio3/public/javascripts/ProductoUtil.js
@@ -1,20 +1,34 @@
 function ProductoUtil() {
 	var URL_BASE = "/productos";
 
-	this.agregar = function(p) {
-		// TODO Implementar
+	this.agregar = function(p, callback) {
+		// Sino coloco JSON.stringify convertirá la data a algo del tipo: nombre=TV&precio=500
+		$.ajax(URL_BASE, {
+			type: "post",
+			contentType: "application/json",
+			data: JSON.stringify(p)
+		}).done(callback);
 	};
 
-	this.modificar = function(p) {
-		// TODO Implementar
+	this.modificar = function(p, callback) {
+		$.ajax(URL_BASE + "/" + p.id, {
+			type: "put",
+			contentType: "application/json",
+			data: JSON.stringify(p)
+		}).done(callback);
 	};
 
-	this.eliminar = function(id) {
-		// TODO Implementar
+	this.eliminar = function(id, callback) {
+		$.ajax(URL_BASE + "/" + id, {
+			type: "delete"
+		}).done(callback);
 	};
 
-	this.obtener = function(id) {
-		// TODO Implementar
+	this.obtener = function(id, callback) {
+		$.ajax(URL_BASE + "/" + id, { 
+			type: 'GET',
+			dataType: 'json' 
+		}).done(callback);
 	};
 
 	this.obtenerTodos = function(llamamePaTras) { 
