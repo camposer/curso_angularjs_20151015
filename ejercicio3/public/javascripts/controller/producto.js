37c3ea36ecc6cc72fa6a6dd83cd78fc27b501e4c
Rodolfo Campos <camposer@gmail.com>
2015-10-27 20:04:44 +0100

    Implementación del CRUD+ de Producto (incluye validaciones personalizadas)

--- ejercicio3/public/javascripts/controller/producto.js
+++ ejercicio3/public/javascripts/controller/producto.js
@@ -6,11 +6,50 @@
 		.controller('ProductoController', [ '$scope', 'ProductoService', ProductoController ]);
 
 	function ProductoController($scope, productoService) {
-		$scope.init = function() {
+		var listar = function() {
 			productoService.obtenerTodos(function(productos) {
 				$scope.productos = productos;
 				$scope.$apply(); // actualiza la vista para los cambios realizados sobre el scope!
 			});
+		};
+
+		var limpiarForm = function() {
+			$scope.producto = {};
+		};
+
+		$scope.init = function() {
+			listar();
+		};
+
+		$scope.guardar = function() {
+			var callback = function() {
+				limpiarForm();
+				listar();
+			};
+
+			$scope.errores = [];
+			if (!$scope.producto.nombre) // TODO mejorar validación
+				$scope.errores.push('Nombre inválido')
+			if (!$scope.producto.precio && !angular.isNumber($scope.producto.precio)) // TODO mejorar validación
+				$scope.errores.push('Precio inválido')
+
+			if ($scope.errores.length == 0) {
+				if ($scope.producto.id) {
+					productoService.modificar($scope.producto, callback);
+				} else {
+					productoService.agregar($scope.producto, callback);
+				}
+			}
+
+		};
+
+		$scope.eliminar = function(id) {
+			if (confirm('Eliminar?'))
+				productoService.eliminar(id, listar);
+		};
+
+		$scope.mostrar = function(p) {
+			$scope.producto = p;
 		};
 	};
 
